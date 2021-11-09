510065610c07472685da01e921c28f05e46dc9f5
jeffrey-hu-53 <jeffrey.hu@stonybrook.edu>
2021-11-08 19:46:07 -0500

    Item Editing
    -Item editing completed
    -undoable and redoable

--- Top5Lists_HW4/client/src/components/Top5Item.js
+++ Top5Lists_HW4/client/src/components/Top5Item.js
@@ -15,6 +15,32 @@ function Top5Item(props) {
     const { store } = useContext(GlobalStoreContext);
     const [editActive, setEditActive] = useState(false);
     const [draggedTo, setDraggedTo] = useState(0);
+    const [text, setText] = useState("");
+
+    function handleToggleEdit(event) {
+        event.stopPropagation();
+        toggleEdit();
+    }
+
+    function toggleEdit() {
+        let newActive = !editActive;
+        if (newActive) {
+            store.setIsItemEditActive();
+        }
+        setEditActive(newActive);
+    }
+
+    function handleKeyPress(event) {
+        if (event.code === "Enter") {
+            let id = event.target.id.substring("item-".length);
+            console.log(id);
+            store.addUpdateItemTransaction(id - 1, text);
+            toggleEdit();
+        }
+    }
+    function handleUpdateText(event) {
+        setText(event.target.value);
+    }
 
     function handleDragStart(event, targetId) {
         event.dataTransfer.setData("item", targetId);
@@ -55,7 +81,7 @@ function Top5Item(props) {
         itemClass = "top5-item-dragged-to";
     }
 
-    return (
+    let cardElement = 
             <ListItem
                 id={'item-' + (index+1)}
                 key={props.key}
@@ -83,13 +109,34 @@ function Top5Item(props) {
                 }}
             >
             <Box sx={{ p: 1 }}>
-                <IconButton aria-label='edit'>
+                <IconButton onClick={handleToggleEdit}  aria-label='edit'>
                     <EditIcon style={{fontSize:'48pt'}}  />
                 </IconButton>
             </Box>
                 <Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
             </ListItem>
-    )
+    if (editActive){
+        cardElement =
+            <TextField
+            margin="normal"
+            required
+            fullWidth
+            id={"item-" + (index + 1)}
+            label="Top 5 Item"
+            name="name"
+            autoComplete="Top 5 Item"
+            className='top5-item'
+            onKeyPress={handleKeyPress}
+            onChange={handleUpdateText}
+            defaultValue={props.text}
+            inputProps={{style: {fontSize: 48}}}
+            InputLabelProps={{style: {fontSize: 24}}}
+            autoFocus
+        />
+    }
+    return (
+        cardElement
+    );
 }
 
 export default Top5Item;
